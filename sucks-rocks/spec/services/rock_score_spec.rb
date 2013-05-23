85e53dabddf605154dfc32984c9b82bc3b9f4a9a
roca <romelcampbell@gmail.com>
2013-05-23 13:52:50 -0400

    add RockScore class

--- sucks-rocks/spec/services/rock_score_spec.rb
+++ sucks-rocks/spec/services/rock_score_spec.rb
@@ -1,12 +1,28 @@
+require_relative "../../app/services/search_engine"
 require_relative "../../app/services/rock_score"
-describe RockScore do 
+
+describe RockScore do
 
 	it "returns 0 for unpopular terms" do
+		SearchEngine.stub(:count_results).with("apple rocks") {0}
+		SearchEngine.stub(:count_results).with("apple sucks") {1}
 		RockScore.for_term("apple").should == 0.0
 	end
-	it "returns 10 for popular terms"
-	it "returns mediocre results for medicore terms"
-	it "does not divide by zero"
-	#10 * positive / (positive + negative)
-	
+	it "returns 10 for popular terms" do
+		SearchEngine.stub(:count_results).with("apple rocks") {1}
+		SearchEngine.stub(:count_results).with("apple sucks") {0}
+		RockScore.for_term("apple").should == 10.0
+	end
+	it "returns mediocre results for medicore terms" do
+		SearchEngine.stub(:count_results).with("apple rocks") {9}
+		SearchEngine.stub(:count_results).with("apple sucks") {11}
+		RockScore.for_term("apple").should == 4.5
+	end
+	it "does not divide by zero"do
+		SearchEngine.stub(:count_results).with("apple rocks") {0}
+		SearchEngine.stub(:count_results).with("apple sucks") {0}
+		RockScore.for_term("apple").should == RockScore::NoScore
+	end
+
+
 end
