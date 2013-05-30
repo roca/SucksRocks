28560269c882f43d02f9de930690d909d1c9f885
roca <RomelCampbell@gmail.com>
2013-05-30 06:02:01 -0400

    Need to stub request for test

--- sucks-rocks/app/services/search_engine.rb
+++ sucks-rocks/app/services/search_engine.rb
@@ -1,14 +1,17 @@
 class SearchEngine
 
 	def self.count_results(query)
-		0
+		search(query)
 	end
 
-	def self.search(term)
+private
+
+	def self.search(query)
 
 		google_books_engine = BentoSearch.get_engine("gbs")
 
-		google_books_engine.search(term, :semantic_search_field => :title).pagination.count
-
+		t = google_books_engine.search(query, :semantic_search_field => :title).pagination.count
+		s = google_books_engine.search(query, :semantic_search_field => :subject).pagination.count
+        return s + t
 	end
 end
--- sucks-rocks/spec/services/search_engine_spec.rb
+++ sucks-rocks/spec/services/search_engine_spec.rb
@@ -1,11 +1,16 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../../config/environment", __FILE__)
+require 'vcr'
 require_relative "../../app/services/search_engine"
 
-describe SearchEngine do
 
+describe SearchEngine do
 	it "counts results" do
+	 VCR.use_cassette("windows-vs-beos") do
 	    windows = SearchEngine.count_results("windows")
 	    beos = SearchEngine.count_results("beos")
 	    windows.should be > beos
+	  end
 	end
 
 end
